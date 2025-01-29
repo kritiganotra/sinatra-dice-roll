83f0016e8604351c46887e9c4743c83c768109ab
kritiganotra <kritiganotra@gmail.com>
2025-01-29 01:06:03 +0000

    Finished dice roll challenge

---
+++ dice.rb
@@ -0,0 +1,57 @@
+require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+get ("/") do
+ erb(:elephant)
+end
+
+#2d6
+get("/dice/2/6") do
+  @rolls = []
+  2.times do 
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+  erb(:two_six)
+end
+
+get("/dice/2/10") do
+  @rolls = []
+  2.times do 
+    die = rand(1..10)
+    @rolls.push(die)
+  end
+  erb(:two_ten)
+end
+
+get("/dice/1/20") do
+  @die = rand(1..20)
+  erb(:one_twenty)
+end
+
+get("/dice/5/4") do
+  @rolls = []
+  5.times do
+    die = rand(1..4)
+    @rolls.push(die)
+  end  
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+    @rolls.push(die)
+  end
+  
+  erb(:one_hundred_six)
+end 
